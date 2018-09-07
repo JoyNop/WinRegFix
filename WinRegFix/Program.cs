82a13c3d2ef07647fd3fa854b75580461355baf4
HanRui <54886036@qq.com>
2018-09-07 14:41:55 +0800

    增加启动管理员

--- WinRegFix/Program.cs
+++ WinRegFix/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace WinRegFix
@@ -13,9 +14,69 @@ namespace WinRegFix
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //处理未捕获的异常   
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            //处理UI线程异常   
+            Application.ThreadException += Application_ThreadException;
+            //处理非UI线程异常   
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+
+            //获得当前登录的Windows用户标示
+            System.Security.Principal.WindowsIdentity identity =
+                System.Security.Principal.WindowsIdentity.GetCurrent();
+            System.Security.Principal.WindowsPrincipal principal =
+                new System.Security.Principal.WindowsPrincipal(identity);
+            //判断当前登录用户是否为管理员
+            if (principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator))
+            {
+                //如果是管理员，则直接运行
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            else
+            {
+                try
+                {
+                    //创建启动对象
+                    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                    //设置运行文件
+                    startInfo.FileName = "RepairMdReg.exe";
+                    //设置启动参数
+                    startInfo.Arguments = "mdreg";
+                    //设置启动动作,确保以管理员身份运行
+                    startInfo.Verb = "runas";
+                    //如果不是管理员，则启动UAC
+                    System.Diagnostics.Process.Start(startInfo);
+                }
+                catch (Exception e)
+                {
+                }
+                finally
+                {
+                    Application.Exit();
+                }
+            }
+        }
+
+
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            var ex = e.Exception;
+            if (ex != null)
+            {
+                MessageBox.Show(ex.Message, "注册表修复工具", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = e.ExceptionObject as Exception;
+            if (ex != null)
+            {
+                MessageBox.Show(ex.Message, "注册表修复工具", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
